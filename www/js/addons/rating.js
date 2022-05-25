067dd05ce9465764e5535e8bb166c909de12c4b9
Aifiq <103562120+Aifiq@users.noreply.github.com>
2022-05-26 04:01:28 +0800

    Add files via upload

---
+++ www/js/addons/rating.js
@@ -0,0 +1,33 @@
+const star = document.getElementById('star');
+
+star.addEventListener('click', function onClick() {
+  star.style.color = 'orange';
+});
+
+
+const star1 = document.getElementById('star1');
+
+star1.addEventListener('click', function onClick() {
+  star1.style.color = 'orange';
+});
+
+
+const star2 = document.getElementById('star2');
+
+star2.addEventListener('click', function onClick() {
+  star2.style.color = 'orange';
+});
+
+
+const star3 = document.getElementById('star3');
+
+star3.addEventListener('click', function onClick() {
+  star3.style.color = 'orange';
+});
+
+
+const star4 = document.getElementById('star4');
+
+star4.addEventListener('click', function onClick() {
+  star4.style.color = 'orange';
+});
